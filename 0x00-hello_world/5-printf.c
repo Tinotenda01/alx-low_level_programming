fa64f3986f464415abbd1e6d93d59b9923bf47f1
Tinotenda01 <chikakatinotendaa@gmail.com>
2022-12-08 08:53:14 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - Entry Point 
+ * main - Entry Point
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("Size of a char: %zu byte(s)\n", sizeof(char));
-	printf("Size of an int: %zu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
