c9ceca84b823fb826e6a4fe89c4c037337625342
Tinotenda01 <chikakatinotendaa@gmail.com>
2022-12-15 11:23:25 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,6 @@
-int _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -8,4 +10,7 @@ void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
+void print_square(int size);
 void print_triangle(int size);
+
+#endif
