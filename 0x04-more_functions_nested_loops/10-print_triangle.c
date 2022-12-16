178e4fe1e8c1876e291a063b448f639351303d90
Tinotenda01 <chikakatinotendaa@gmail.com>
2022-12-15 17:43:32 -0800

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,7 +14,7 @@ void print_triangle(int size)
 	{
 		for (inc1 = 1; inc1 <= size; inc1++)
 		{
-			for ((inc2 = size - inc1); inc2 > 0; inc2--)
+			for ((inc2 = size - inc1); inc2 > 0; inc2++)
 			{
 				putchar(' ');
 			}
@@ -28,7 +28,6 @@ void print_triangle(int size)
 			{
 				continue;
 			}
-
 			putchar('\n');
 		}
 	}
