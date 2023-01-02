4e070fbb520cd34a54a1dc5d5bb89ca03565bd1d
Tinotenda01 <chikakatinotendaa@gmail.com>
2023-01-02 14:53:19 -0800

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strchr - locate characters in string
+ *
+ * @s: source of string
+ * @c: character to find
+ *
+ * Return: the string from character found
+ */
+
+char *_strchr(char *s, char c)
+{
+	int a = 0, b;
+
+	while (s[a])
+	{
+		a++;
+	}
+
+	for (b = 0; b <= a; b++)
+	{
+		if (c == s[b])
+		{
+			s += b;
+			return (s);
+		}
+	}
+
+	return ('\0');
+}
+
