4f61dd64ec9aca7a0ab01cad3f0d18f0b4e990cc
Tinotenda01 <chikakatinotendaa@gmail.com>
2023-01-04 12:59:10 -0800

    0-puts_recursion.c

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - writes c  character to stdout
+ * @c: The character to be printed
+ *
+ * Return: On success 1
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
